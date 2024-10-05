e57472b672784c0d48ced46c0c006c144fc09f6c
Abhishek kumar <abhisheksinghpatna555@gmail.com>
2024-10-05 21:16:06 +0530

    Add files via upload

---
+++ src/components/LinksArray.jsx
@@ -0,0 +1,418 @@
+export const links = [
+    {
+        name: 'New-Delhi',
+        submenu: true,
+        country: [
+            {
+                sublink: [
+                    {name: "India", link: "/"}
+                ],
+            },
+        ],
+        nearbyCity: [
+            {
+                Head: "Nearby Cities",
+                sublink: [
+                    {name: "Gurgaon", link: "/"},
+                    {name: "Greater Noida", link: "/"},
+                    {name: "Ghaziabad", link: "/"},
+                    {name: "Noida", link: "/"}
+                ],
+            },
+        ],
+        popularCity: [
+            {
+                Head: "Popular Cities",
+                sublink: [
+                    {name: "Ahmedabad", link:"/"},
+                    {name: "Banglore", link:"/"},
+                    {name: "Beyond Thane", link:"/"},
+                    {name: "Chennai", link:"/"},
+                    {name: "Gurgaon", link: "/"},
+                    {name: "Hyderabad", link:"/"},
+                    {name: "Indore", link:"/"},
+                    {name: "Jaipur", link:"/"},
+                    {name: "Kolkata", link:"/"},
+                    {name: "Lucknow", link:"/"},
+                    {name: "Mumbai", link:"/"},
+                    {name: "Namvi Mumbai", link:"/"},
+                    {name: "New Delhi", link:"/"},
+                    {name: "Noida", link:"/"},
+                    {name: "Pune", link:"/"},
+                    {name: "Thane", link:"/"}
+                ],
+            },
+            {
+                Head: "Other Cities",
+                sublink: [
+                    {name: "Agra", link:"/"},
+                    {name: "Ahmadanagar", link:"/"},
+                    {name: "Allahabad", link:"/"},
+                    {name: "Aluva", link:"/"},
+                    {name: "Amritsar", link:"/"},
+                    {name: "Aurangabad", link:"/"},
+                    {name: "Badlapur", link:"/"},
+                    {name: "Bareilly", link: "/"},
+                    {name: "Belgaum", link:"/"},
+                    {name: "Bhiwadi", link:"/"},
+                    {name: "Bhiwandi", link:"/"},
+                    {name: "Bhopal", link:"/"},
+                    {name: "Bhubneswar", link:"/"},
+                    {name: "Bokaro Steel City", link:"/"},
+                    {name: "Chandigarh", link:"/"},
+                    {name: "Chengalpattu", link:"/"},
+                    {name: "Coimbatore",link:"/"},
+                    {name: "Dehradun",link:"/"},
+                    {name: "Durgapur",link:"/"},
+                    {name: "Ernakulam",link:"/"},
+                    {name: "Erode",link:"/"},
+                    {name: "Faridabad",link:"/"},
+                    {name: "Ghaziabad",link:"/"},
+                    {name: "Goa",link:"/"},
+                    {name: "Gorakhpur",link:"/"},
+                    {name: "Greater Noida",link:"/"},
+                    {name: "Guntur",link:"/"},
+                    {name: "Guwahati",link:"/"},
+                    {name: "Gwalior",link:"/"},
+                    {name: "Haridwar",link:"/"},
+                    {name: "Hosur",link:"/"},
+                    {name: "Hubli",link:"/"},
+                    {name: "Jabalpur",link:"/"},
+                    {name: "Jalandhar",link:"/"},
+                    {name: "Jammu",link:"/"},
+                    {name: "Jamshedpur",link:"/"},
+                    {name: "Jodhpur",link:"/"},
+                    {name: "Kalyan",link:"/"},
+                    {name: "Kannur",link:"/"},
+                    {name: "Kanpur",link:"/"},
+                    {name: "Khopoli",link:"/"},
+                    {name: "Kochi",link:"/"},
+                    {name: "Kodaikanal",link:"/"},
+                    {name: "Kottayam",link:"/"},
+                    {name: "kozhikode",link:"/"},
+                    {name: "Lonavala",link:"/"},
+                    {name: "Ludhiana",link:"/"},
+                    {name: "Madurai",link:"/"},
+                    {name: "Mangalore",link:"/"},
+                    {name: "Mohali",link:"/"},
+                    {name: "Mysore",link:"/"},
+                    {name: "Nagpur",link:"/"},
+                    {name: "Nainital",link:"/"},
+                    {name: "Nanded",link:"/"},
+                    {name: "Nashik",link:"/"},
+                    {name: "Navsari",link:"/"},
+                    {name: "Nellore",link:"/"},
+                    {name: "Newtown",link:"/"},
+                    {name: "Ooty",link:"/"},
+                    {name: "Palakkad",link:"/"},
+                    {name: "Palghar",link:"/"},
+                    {name: "Punchkula",link:"/"},
+                    {name: "Patiala",link:"/"},
+                    {name: "Patna",link:"/"},
+                    {name: "Pondicherry",link:"/"},
+                    {name: "Raipur",link:"/"},
+                    {name: "Rajahmundry",link:"/"},
+                    {name: "Ranchi",link:"/"},
+                    {name: "Salem",link:"/"},
+                    {name: "Satara",link:"/"},
+                    {name: "Shimla",link:"/"},
+                    {name: "Siliguri",link:"/"},
+                    {name: "Solapur",link:"/"},
+                    {name: "Sonipat",link:"/"},
+                    {name: "Surat",link:"/"},
+                    {name: "Thanjavur",link:"/"},
+                    {name: "Thrissur",link:"/"},
+                    {name: "Tirunelveli",link:"/"},
+                    {name: "Tirupati",link:"/"},
+                    {name: "Tirupur",link:"/"},
+                    {name: "Trichy",link:"/"},
+                    {name: "Trivandrum",link:"/"},
+                    {name: "Tumkur",link:"/"},
+                    {name: "Udaipur",link:"/"},
+                    {name: "Udupi",link:"/"},
+                    {name: "Vadodara",link:"/"},
+                    {name: "Vapi",link:"/"},
+                    {name: "Varanasi",link:"/"},
+                    {name: "Vijayawada",link:"/"},
+                    {name: "Vishakhapatnam",link:"/"},
+                    {name: "Vrindavan",link:"/"},
+                    {name: "Zirakpur",link:"/"},
+                ],
+            },
+        ],
+    },
+];
+
+export const buy_options = [
+    {
+        Head: 'Popular Choices',
+        sublink: [
+            {name: 'Ready to move', link: '/'},
+            {name: 'Owner Properties', link: '/'},
+            {name: 'Budget Homes', link: '/'},
+            {name: 'Premium Homes', link: '/'},
+            {name: 'New Projects', sName: 'new', link: '/'},
+        ],
+    },
+    {
+        Head: 'Properties Types',
+        sublink: [
+            {name: 'Flats in New-Delhi', link: '/'},
+            {name: 'House for sale in New...', link: '/'},
+            {name: 'Villa in New-Delhi', link: '/'},
+            {name: 'Plots in New-Delhi', link: '/'},
+            {name: 'Office in New_Delhi', link: '/'},
+            {name: 'Commercial Space in ...', link: '/'},
+        ],
+    },
+    {
+        Head: 'Budget',
+        sublink: [
+            {name: 'Under ₹ 50 Lac', link: '/'},
+            {name: '₹ 50 Lac - ₹ 1 Cr', link: '/'},
+            {name: '₹ 1 Cr - ₹ 1.5 Cr', link: '/'},
+            {name: 'Above ₹ 1.5 Cr', link: '/'},
+        ],
+    },
+    {
+        Head: 'Explore',
+        sublink: [
+            {name: 'Lacalites in New-Delhi', link: '/'},
+            {name: 'Projects in New-Delhi', link: '/'},
+            {name: 'Investment Hotspot', link: '/'},
+            {name: 'Find an Agent', link: '/'},
+            {name: 'Home Interiors in Ne...', sName: 'new', link: '/'},
+        ],
+    },
+    {
+        Head: 'Buying Tools',
+        sublink: [
+            {name: 'Prop Worth', link: '/'},
+            {name: 'Rates & Trends', link: '/'},
+            {name: 'Buy vs Rent', link: '/'},
+            {name: 'Tips and Guides', link: '/'},
+        ],
+    },
+];
+
+export const rent_options = [
+    {
+        Head: 'Popular Choices',
+        sublink: [
+            {name: 'Owner Properties', link: '/'},
+            {name: 'Verified Properties', link: '/'},
+            {name: 'Furnished Homes', link: '/'},
+            {name: 'Bachelor Friendly Ho...', link: '/'},
+            {name: 'Immediately Available', link: '/'},
+        ],
+    },
+    {
+        Head: 'Properties Types',
+        sublink: [
+            {name: 'Flat for rent in New-Delhi', link: '/'},
+            {name: 'House for rent in New...', link: '/'},
+            {name: 'Villa for rent in New-D...', link: '/'},
+            {name: 'PG in New-Delhi', link: '/'},
+            {name: 'Office Space in New-...', link: '/'},
+            {name: 'Commercial Space in', link: '/'},
+            {name: 'Coworking Space in New...', link: '/'},
+            {name: 'Coliving Space in New...', link: '/'},
+            {name: 'Student Hostels in New...', link: '/'},
+            {name: 'Luxury PG in New-Del...', link: '/'},
+        ],
+    },
+    {
+        Head: 'Budget',
+        sublink: [
+            {name: 'Under ₹ 10,000', link: '/'},
+            {name: '₹ 10,000 - ₹ 15,000', link: '/'},
+            {name: '₹ 15,000 - ₹ 25,000', link: '/'},
+            {name: 'Above ₹ 25,000', link: '/'},
+        ],
+    },
+    {
+        Head: 'Explore',
+        sublink: [
+            {name: 'Localities', link: '/'},
+            {name: 'Buy vs rent', link: '/'},
+            {name: 'Find an Agent', link: '/'},
+            {name: 'Share Requirement', link: '/'},
+            {name: 'Properties Services', link: '/'},            
+            {name: 'Rent Agreement', link: '/'},
+        ],
+    },
+    {
+        Head: 'Buying Tools',
+        sublink: [
+            {name: 'Prop Worth', link: '/'},
+            {name: 'Rates & Trends', link: '/'},
+            {name: 'Buy vs Rent', link: '/'},
+            {name: 'Tips and Guides', link: '/'},
+        ],
+    },
+];
+
+export const sell_options = [
+    {
+        Head: 'For Agent & Builder',
+        sublink: [
+            {name: 'My Dashboard', link: '/'},
+            {name: 'Ad Packages', link: '/'},
+            {name: 'Advantage', link: '/'},
+            {name: 'Developer Lounge', link: '/'},
+            {name: 'Sales Enquiry', link: '/'},
+        ],
+    },
+    {
+        Head: 'Selling Tools',
+        sublink: [
+            {name: 'Property Evaluation', link: '/'},
+            {name: 'Find an Agent', link: '/'},
+            {name: 'Rates & Trends', link: '/'},
+            {name: 'Prop Worth', link: '/'},
+        ],
+    },
+];
+
+export const homeLoan_options = [
+    {
+        Head: 'Apply Now',
+        sublink: [
+            {name: 'Home Loans', link: '/'},
+            {name: 'Balance Transfer', link: '/'},
+            {name: 'Loan Against', link: '/'},
+            {name: 'Property', link: '/'},
+        ],
+    },
+    {
+        Head: 'Partners',
+        sublink: [
+            {name: 'SBI Home Loan', link: '/'},
+            {name: 'HDFC Ltd Home', link: '/'},
+            {name: 'Loan', link: '/'},
+            {name: 'Axis Home Loan', link: '/'},
+            {name: 'Kotak Home Loan', link: '/'},
+            {name: 'LIC HF Home Loan', link: '/'},
+        ],
+    },
+    {
+        Head: 'Explore',
+        sublink: [
+            {name: 'Home Loan EMI Calculator', link: '/'},
+            {name: 'Home EMI Calculator', link: '/'},
+            {name: 'Get Home Loan Offers', sName: 'new', link: '/'},
+            {name: 'Check Credit Score', link: '/'},
+            {name: 'Home Loan Repayment'},
+            {name: 'Home Loan Interest Rate', link: '/'},
+            {name: 'Home Loan Balance', link: '/'},
+            {name: 'Transfer', link: '/'},
+            {name: 'Home Loan Documentation', link: '/'},
+        ],
+    },
+    {
+        Head: 'EMI Calculator',
+        sublink: [
+            {name: 'SBI Home Loan EMI Calculator', link: '/'},
+            {name: 'HDFC Home EMI Calculator', link: '/'},
+            {name: 'Axis Bank Home Loan EMI Calculator', link: '/'},
+            {name: 'Calculator', link: '/'},
+            {name: 'Bajaj Home Loan EMI Calculator'},
+            {name: 'Kotak Home Loan EMI Calculator', link: '/'},
+            {name: 'L&T Home Loan EMI Calculator', link: '/'},
+        ],
+    },
+    {
+        Head: 'Interest Rates',
+        sublink: [
+            {name: 'SBI Home Loan Interest Rate', link: '/'},
+            {name: 'HDFC Home Interest Rate', link: '/'},
+            {name: 'Axis Bank Home Loan Interest Rate', link: '/'},
+            {name: 'Rate', link: '/'},
+            {name: 'Bajaj Home Loan Interest Rate'},
+            {name: 'Kotak Home Loan Interest Rate', link: '/'},
+            {name: 'L&T Home Loan Interest Rate', link: '/'},
+        ],
+    },
+];
+
+export const homeInterior_options = [
+    {
+        Head: 'Home Interior Design Ideas',
+        sublink: [
+            {name: 'Modular Integer Design Idea', link: '/'},
+            {name: 'False Ceiling Design Ideas', link: '/'},
+            {name: 'Bathroom Design Ideas', link: '/'},
+            {name: 'Bedroom Design Ideas', link: '/'},
+            {name: 'TV Unit Design Ideas', link: '/'},
+        ],
+    },
+    {
+        Head: 'Full Home Interior Designs',
+        sublink: [
+            {name: '1BHK Interior Design', link: '/'},
+            {name: '2BHK Interior Design', link: '/'},
+            {name: '3BHK Interior Design', link: '/'},
+            {name: '4BHK Interior Design', link: '/'},
+        ],
+    },
+    {
+        Head: 'Explore Our Services',
+        sublink: [
+            {name: 'Home Interior Design Services', link: '/'},
+            {name: 'Interior Designers in New-Delhi', link: '/'},
+            {name: 'Design Consultation', sName: 'new', link: '/'},
+            {name: 'Full Home Interior Cost Calculator', link: '/'},
+            {name: 'Kitchen / Wardrobe Calculator'},
+        ],
+    },
+];
+
+export const advice_options = [
+    {
+        Head: 'MB Research',
+        sublink: [
+            {name: 'Insights', link: '/'},
+            {name: 'Research', link: '/'},
+            {name: 'Prop Index', link: '/'},
+            {name: 'Find Pincode', link: '/'},
+        ],
+    },
+    {
+        Head: 'Services & Tools',
+        sublink: [
+            {name: 'Property Valuation', sName: 'new', link: '/'},
+            {name: 'Rates & Trends', link: '/'},
+            {name: 'Area Converter', link: '/'},
+            {name: 'PropWorth', link: '/'},
+            {name: 'Buy v/s Rent', link: '/'},
+        ],
+    },
+    {
+        Head: 'Localities & Projects',
+        sublink: [
+            {name: 'Localities in New-Delhi', link: '/'},
+            {name: 'Locality Review Videos', link: '/'},
+            {name: 'Compare Localities', link: '/'},
+            {name: 'New Projects in New-De...', link: '/'},
+            {name: 'Project Review Videos', link: '/'},
+            {name: 'MBTV Videos', link: '/'},
+        ],
+    },
+    {
+        Head: 'News & Blogs',
+        sublink: [
+            {name: 'Latest Blogs', sName: 'new', link: '/'},
+            {name: 'Lifestyle', link: '/'},
+            {name: 'Policies Localities', link: '/'},
+            {name: 'Finance & Legal', link: '/'},
+            {name: 'City Blogs', link: '/'},
+            {name: 'Property News', link: '/'},
+            {name: 'Trending Web Stories', link: '/'},
+        ],
+    },
+];
+
+export const help_options = [
+    {name: 'Help Center', link: '/'},
+    {name: 'Sales Enquiry', link: '/'},
+]
