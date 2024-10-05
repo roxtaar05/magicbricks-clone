d43e160ab10c324b17ca42966b02b641d9c91aac
Abhishek kumar <abhisheksinghpatna555@gmail.com>
2024-10-05 21:17:49 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Main_Body from "./components/Body/Main_Body"
+import { Footer } from "./components/Footer/Footer"
+import Header from "./components/Header/Header"
+
+const App = () => {
+  return (
+      <div>
+        <Header />
+        <Main_Body />
+        <Footer />
+      </div>
+  )
+}
+
+export default App
